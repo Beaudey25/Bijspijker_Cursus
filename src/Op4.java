9d028642a7f47a1729719ec21eeb23796266eff0
beaudey <2110836@student.rocvf.nl>
2020-02-27 13:19:22 +0100

    .

---
+++ src/Op4.java
@@ -0,0 +1,26 @@
+import sun.text.normalizer.UCharacter;
+
+import java.util.Scanner;
+
+public class Op4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Van welk karFakter wil je de ASCII waarde weten?");
+        char userinput = scanner.nextLine().charAt(0);
+        int waarde = (int) userinput;
+        System.out.println("De ASCII waarde van het karakter " + userinput + " is " + waarde + "");
+
+
+        System.out.println("Van welk karakter wil je weten of het een getal of letter is?");
+        userinput = scanner.nextLine().charAt(0);
+        if (Character.isLetter(userinput)); {
+            
+        }
+
+
+
+    }
+
+}

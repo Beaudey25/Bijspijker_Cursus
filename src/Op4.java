74cd6b7f674b264bdf41826735a8938430ba1e93
beaudey <2110836@student.rocvf.nl>
2020-02-27 14:31:55 +0100

    .

--- src/Op4.java
+++ src/Op4.java
@@ -1,3 +1,4 @@
+import com.sun.deploy.security.SelectableSecurityManager;
 import sun.text.normalizer.UCharacter;
 
 import java.util.Scanner;
@@ -7,6 +8,7 @@ public class Op4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
         System.out.print("Van welk karFakter wil je de ASCII waarde weten?");
         char userinput = scanner.nextLine().charAt(0);
         int waarde = (int) userinput;
@@ -15,12 +17,14 @@ public class Op4 {
 
         System.out.println("Van welk karakter wil je weten of het een getal of letter is?");
         userinput = scanner.nextLine().charAt(0);
-        if (Character.isLetter(userinput)); {
-            
+        if (Character.isLetter(userinput)) {
+            System.out.println("De " + userinput + " is een letter");
+        } else if (Character.isDigit(userinput)) {
+        System.out.println("De " + userinput + " is een getal.");
+        } else {
+            System.out.println("De " + userinput + " is geen letter en ook geen gertal");
         }
 
 
-
     }
-
 }

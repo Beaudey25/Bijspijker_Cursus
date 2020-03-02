cf7ce58582c64f0cb08938f3c2102b0813d11682
beaudey <2110836@student.rocvf.nl>
2020-03-02 10:58:39 +0100

    .

--- src/Op4.java
+++ src/Op4.java
@@ -20,11 +20,42 @@ public class Op4 {
         if (Character.isLetter(userinput)) {
             System.out.println("De " + userinput + " is een letter");
         } else if (Character.isDigit(userinput)) {
-        System.out.println("De " + userinput + " is een getal.");
+            System.out.println("De " + userinput + " is een getal.");
         } else {
             System.out.println("De " + userinput + " is geen letter en ook geen gertal");
         }
 
 
+        System.out.println("Van welk karakter wil je weten of het een getal of letter is?");
+        userinput = scanner.nextLine().toLowerCase().charAt(0);
+        if (userinput == 'a' || userinput == 'e' || userinput == 'i' || userinput == 'u' || userinput == 'o') {
+            System.out.println("De " + userinput + " is een klinker");
+        } else {
+            System.out.println("De " + userinput + " is een medeklinker");
+        }
+
+
+        System.out.println("Van welke letter wil je het nummer weten?");
+        userinput = scanner.nextLine().toLowerCase().charAt(0);
+        if (userinput == 'a' || userinput == 'b' || userinput == 'c' ) {
+            System.out.println("De " + userinput + " staat op nummer 2");
+        } else if (userinput == 'd' || userinput == 'e' || userinput == 'f') {
+            System.out.println("De " + userinput + " staat op nummer 3");
+        } else if (userinput == 'g' || userinput == 'h' || userinput == 'i') {
+        System.out.println("De " + userinput + " staat op nummer 4");
+        } else if (userinput == 'j' || userinput == 'k' || userinput == 'l') {
+            System.out.println("De " + userinput + " staat op nummer 5");
+        } else if (userinput == 'm' || userinput == 'n' || userinput == 'o') {
+            System.out.println("De " + userinput + " staat op nummer 6");
+        } else if (userinput == 'p' || userinput == 'q' || userinput == 'r' || userinput == 's') {
+            System.out.println("De " + userinput + " staat op nummer 7");
+        } else if (userinput == 't' || userinput == 'u' || userinput == 'v') {
+            System.out.println("De " + userinput + " staat op nummer 8");
+        } else if (userinput == 'w' || userinput == 'x' || userinput == 'y' || userinput == 'z') {
+            System.out.println("De " + userinput + " staat op nummer 9");
+        }
+
+
     }
 }
+
